67be7339b781c5775747365ad222387344edcd52
ahmadyazdi243 <ahmadyazdi243@gmail.com>
2025-11-30 00:57:00 +0330

    imp 2

--- src/main/java/com/yazdi/queue_management/config/ConsumerConfiguration.java
+++ src/main/java/com/yazdi/queue_management/config/ConsumerConfiguration.java
@@ -6,9 +6,11 @@ import org.apache.kafka.common.serialization.StringDeserializer;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
 
 import java.util.Properties;
 
+@EnableKafka
 @Configuration
 public class ConsumerConfiguration {
 
@@ -20,7 +22,8 @@ public class ConsumerConfiguration {
     public KafkaConsumer<String, String> customConsumer(){
         Properties props = new Properties();
         props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
-        props.put(ConsumerConfig.GROUP_ID_CONFIG, "manual-consumer");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "manual-consumer-group");
+//        props.put(ConsumerConfig.CLIENT_ID_CONFIG, "manual-consumer");
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
--- src/main/java/com/yazdi/queue_management/config/ManualConsumerStarter.java
+++ src/main/java/com/yazdi/queue_management/config/ManualConsumerStarter.java
@@ -1,17 +1,25 @@
 package com.yazdi.queue_management.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
 import org.springframework.stereotype.Component;
 
 import java.time.Duration;
 import java.util.List;
 
+@Slf4j
 @Component
 public class ManualConsumerStarter {
+    
     public ManualConsumerStarter(KafkaConsumer<String, String> consumer) {
         consumer.subscribe(List.of("manual-topic"));
-        // warm-up poll (non-blocking) to kick group join
-        consumer.poll(Duration.ofMillis(100));
+        // small warm-up to kick the group
+        try {
+            consumer.poll(Duration.ofMillis(100));
+        } catch (Exception e) {
+            log.error(e.getMessage());
+            // ignore during startup
+        }
     }
 
 }
